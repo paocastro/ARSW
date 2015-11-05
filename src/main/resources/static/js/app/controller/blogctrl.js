efdd60dca7f190f9fe347ab506963ac58a4228be
paocastro <ipao526@gmail.com>
2015-11-05 12:55:07 -0500

    blogctrl

---
+++ src/main/resources/static/js/app/controller/blogctrl.js
@@ -0,0 +1,40 @@
+var app = angular.module("app",[]);
+
+app.controller("blogCtrl", function($scope,$log,$http) {
+    $scope.entry = {title : "Title",
+                    content : "Content"};
+    $scope.entries = [];
+    $log.debug('se creo el $scope');
+
+    $scope.loadData = function() {
+        var configList = {
+                method: "GET",
+                url: "blogs"
+                    };
+
+        var response=$http(configList);
+
+        response.success(function(data, status, headers, config) {
+            $scope.entries = data;
+            });
+
+        response.error(function(data, status, headers, config) {
+            alert("Ha fallado la petición. Estado HTTP:"+status);
+            });
+    };
+    $scope.loadData();
+
+ $scope.processForm = function() {
+        $log.debug($scope.entry);
+        $http({
+            method  : 'POST',
+            url     : 'blog',
+            data    : $scope.entry
+        }).success(function(data) {
+            console.log(data);
+            $scope.loadData();
+        });
+    };
+});
+
+
